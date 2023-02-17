c3b2b94c4c21fe4fe9f7e5da2f15043a13e92931
Treysuarez <treyxsuarez@gmail.com>
2023-02-17 19:16:07 +0000

    added user questions to index.js file

--- starter/index.js
+++ starter/index.js
@@ -5,7 +5,55 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
 
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a short description of your project: "
+
+    },
+
+    {
+        type: "input",
+        name: "installion",
+        message: "Please provide information on how to use your project: "
+    },
+
+    {
+        type: "list",
+        name: "license",
+        message: "Which license would you like to use for your project?",
+        choices: ["MIT", "GPLv3", "Apache 2.0", "BSD 3", "Unlicense"]
+    },
+
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please provide guidelines for how a user can contribute to your project: "
+    },
+
+    {
+        type: "input",
+        name: "tests",
+        message: "Please provide instructions for how to run test for your project: "
+    },
+
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?"
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    }
 ];
 
 // function to write README file
