d02d30849bd0058b1ee4a8be37d1eb542fb8c519
Treysuarez <treyxsuarez@gmail.com>
2023-02-21 18:02:35 +0000

    updated index for error and added code to markdown

--- starter/index.js
+++ starter/index.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
+const util = require("util")
+const writeFileAsync = util.promisify(fs.writeFile);
 const generateMarkdown = require("./utils/generateMarkdown");
+let licenses = ["MIT","Apache 2.0 License","Eclipse Public License 1.0"]
 
 // array of questions for user
 
@@ -9,78 +12,85 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // inquirer
 //   .prompt([
 
-
- const questions = [   
-    {
-        type: "input",
-        name: "title",
-        message: "What is the title of your project?"
+const questions = [   
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please provide a short description of your project:"
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please provide instructions for how to install your project:"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please provide information on how to use your project:"
+  },
+  {
+    type:"list",
+        name:"license",
+        message:"Pick the license name from this list: ",
+        choices: licenses,
+        filter: function(val){
+            if(val== "MIT"){
+                
+                return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+            } else if( val == "Apache 2.0 License"){
+                return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+            }else if (val == "Eclipse Public License 1.0"){
+                return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)"
+            }
+        }
     },
-
-    {
-        type: "input",
-        name: "description",
-        message: "Please provide a short description of your project: "
-
-    },
-
-    {
-        type: "input",
-        name: "installion",
-        message: "Please provide information on how to use your project: "
-    },
-
-    {
-        type: "list",
-        name: "license",
-        message: "Which license would you like to use for your project?",
-        choices: ["MIT", "GPLv3", "Apache 2.0", "BSD 3", "Unlicense"]
-    },
-
-    {
-        type: "input",
-        name: "contributing",
-        message: "Please provide guidelines for how a user can contribute to your project: "
-    },
-
-    {
-        type: "input",
-        name: "tests",
-        message: "Please provide instructions for how to run test for your project: "
-    },
-
-    {
-        type: "input",
-        name: "github",
-        message: "What is your GitHub username?"
-    },
-
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email address?"
-    }
+  {
+    type: "input",
+    name: "contributing",
+    message: "Please provide guidelines for how a user can contribute to your project:"
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Please provide instructions for how to run tests for your project:"
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?"
+  }
 ];
-
-
 
 // function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        if(err) throw err;
-        console.log('SampleREADME.md file successfully created!');
-    });
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log(`SampleREADME.md file successfully created!`);
+  });
 }
 
 // function to initialize program
 function init() {
-    inquirer.prompt(questions)
-    .then((answers) => {
-        const markdown = generateMarkdown(answers);
-        writeToFile("SampleREADME.md", markdown);
-    })
-    .catch((err) => console.error(err));
+  inquirer.prompt(questions)
+  .then((answers)=> {
+    if(answers.licenses== 'MIT'){
+        
+        answers["licenseBadge"] = `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+    }
+    writeToFile("SampleREADME.md", generateMarkdown(answers));
+}) 
 }
 
 // function call to initialize program
 init();
+
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,6 +1,42 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+ ## Table of Contents  
+ 1. [Description](#description)
+ 2. [Installation](#installation)
+ 3. [Usage](#usage)
+ 4. [Contributing](#contributing)
+ 5. [Testing](#testing)
+ 6. [Questions](#questions)
+ 7. [License](#license)
+ 
+ ## Description
+ ${data.description}
+
+ ## Installation
+ ${data.installation}
+ 
+ ## Usage
+ To use the app:  
+ ${data.usage}
+ 
+ ## Contributing
+ If you wish to contribute:  
+ ${data.contributing}
+
+ ## Testing
+ To test the application: 
+ ${data.tests}
+
+ ## Questions
+ Feel free to contact me if you have any questions! 
+ Reach me via email: ${data.email}  
+
+ My GitHub profile:
+ [TreyS Profile](https://github.com/${data.github})
+ 
+ ## License
+ ${data.license} 
 
 `;
 }
